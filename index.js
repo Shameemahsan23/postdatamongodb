fbcb569a1a031becc8afe22da2a593537b08f8fa
md shameem ahsan <80584421+Shameemahsan23@users.noreply.github.com>
2023-08-24 01:57:40 +0600

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,26 @@ app.post('/api/save-data', async (req, res) => {
   }
 });
 
+app.put('/api/items/:id', async (req, res) => {
+  try {
+    const itemId = req.params.id;
+    const { name, description } = req.body;
+
+    // Find the item by ID and update its properties
+    const updatedItem = await Item.findByIdAndUpdate(itemId, { name, description }, { new: true });
+
+    if (!updatedItem) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+
+    return res.json(updatedItem);
+  } catch (error) {
+    console.error('Error updating item:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
